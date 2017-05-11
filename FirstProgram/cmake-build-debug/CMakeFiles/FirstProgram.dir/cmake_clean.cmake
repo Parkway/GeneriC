2ca54bdc9c8aaf6ec69e3e1172367f11895373a5
Lance <ldouglastech@gmail.com>
2017-05-11 14:11:55 -0400

    C is hard.

---
+++ FirstProgram/cmake-build-debug/CMakeFiles/FirstProgram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FirstProgram.dir/main.c.o"
+  "FirstProgram.pdb"
+  "FirstProgram"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FirstProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GeneriC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GeneriC.dir/main.c.o"
+  "GeneriC.pdb"
+  "GeneriC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GeneriC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
