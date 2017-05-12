6051624178daeb854697eff8b318d2d7ab3f8df1
Lance <ldouglastech@gmail.com>
2017-05-11 23:12:28 -0400

    Crap

---
+++ Switch/cmake-build-debug/CMakeFiles/Switch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Switch.dir/main.c.o"
+  "Switch.pdb"
+  "Switch"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Switch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
