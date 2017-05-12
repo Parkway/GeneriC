476acd0369a3a43060009149fd2be88462e70080
Lance <ldouglastech@gmail.com>
2017-05-11 21:42:59 -0400

    Added while loop class(?)

---
+++ While/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int rocketShip = 3;
+    while (rocketShip >= 0) {
+        if (rocketShip==0) {
+            printf("Blast Off!\n");
+        }
+        else {
+            printf("%d\n", rocketShip);
+        }
+        rocketShip--;
+    }
+    return 0;
+}
