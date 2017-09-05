aed896466256bdeec239d3ecc1f31118fcd9010d
Parkway <ldouglastech@gmail.com>
2017-09-05 14:16:25 -0400

    Removing unnecessary whitespace

--- IfElse/main.c
+++ IfElse/main.c
@@ -2,16 +2,13 @@
 
 int main() {
     int num;
-
     //Just an example of odd or even if else.
     printf("Enter a number: ");
     scanf("%d", &num);
-
     if (num % 2) {
         printf("Number is odd.");
     } else {
         printf("Number is even.");
     }
-
     return 0;
 }
