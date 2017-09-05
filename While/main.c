43ecb39e4bc969154cf7a01e0d39fc678952b99f
Parkway <ldouglastech@gmail.com>
2017-09-05 14:34:53 -0400

    Added execution delay to loop. Much more accurate now.

--- While/main.c
+++ While/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
     int rocketShip = 3;
     while (rocketShip >= 0) {
+        sleep(1);
         if (rocketShip==0) {
             printf("Blast Off!\n");
         }
