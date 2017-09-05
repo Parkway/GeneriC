d22d083013a79da8632688aae3bc6d4a94b705f4
Parkway <ldouglastech@gmail.com>
2017-09-05 14:43:54 -0400

    Git issue resolution.

---
+++ ForLoop/cmake-build-debug/CMakeFiles/ForLoop.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ForLoop.dir/main.c.obj"
+  "ForLoop.pdb"
+  "ForLoop.exe"
+  "ForLoop.exe.manifest"
+  "libForLoop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ForLoop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
