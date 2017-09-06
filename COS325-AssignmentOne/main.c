4180d2b9c8607e706dd9ec9284d502be87d64884
Parkway <ldouglastech@gmail.com>
2017-09-05 23:30:14 -0400

    First Assignment is Live!

---
+++ COS325-AssignmentOne/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * This is merely a placeholder for GitHub
+ * until I actually come up with
+ * something to use for my first genuine
+ * C assignment.
+ */
+int main() {
+    printf("Hello, GitHub!\n");
+    return 0;
+}
