72daf9f66e14cd0e5f80ae7c65f47c5a0f632e84
Parkway <ldouglastech@gmail.com>
2017-09-07 11:17:50 -0400

    I did a lot without commiting.

---
+++ COS325-AssignmentOne/cmake-build-debug/CMakeFiles/COS325_AssignmentOne.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COS325_AssignmentOne.dir/main.c.obj"
+  "COS325_AssignmentOne.pdb"
+  "COS325_AssignmentOne.exe"
+  "COS325_AssignmentOne.exe.manifest"
+  "libCOS325_AssignmentOne.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/COS325_AssignmentOne.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mathMachine/cmake-build-debug/CMakeFiles/mathMachine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mathMachine.dir/main.c.obj"
+  "mathMachine.pdb"
+  "mathMachine.exe"
+  "mathMachine.exe.manifest"
+  "libmathMachine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mathMachine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
